a563d751fc285e3c88e9a53731e69c22c84902c3
imran7541 <imrandt2001@gmail.com>
2026-05-03 05:46:37 -0600

    add a desgin

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar/>
-       <main className="mx-w-7xl mx-auto"> {children}</main>
+       <main className="max-w-7xl mx-auto px-4"> {children}</main>
         <Footer/>
 
       </body>
