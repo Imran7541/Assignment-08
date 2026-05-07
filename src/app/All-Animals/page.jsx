e79b5776fd4170ba4faf47c839a718aeb911b443
imran7541 <imrandt2001@gmail.com>
2026-05-07 10:11:14 -0600

    add a all animals

--- src/app/All-Animals/page.jsx
+++ src/app/All-Animals/page.jsx
@@ -1,13 +1,15 @@
 import AnimalCard from "@/components/AnimalCard";
 
-
+export const dynamic = 'force-dynamic';
 const AllAnimalsPage =async () => {
-    const res =await fetch('https://assignment-08-f9oc.vercel.app/data.json')
+    const res =await fetch('https://assignment-08-f9oc.vercel.app/data.json',{
+        cache: 'no-store'
+    })
    const photos= await res.json();
    
     return (
         <div>
-            <h1 className="text-2xl font-bold">All Photos</h1>
+            <h1 className="text-2xl font-bold">All Animals</h1>
             <div className="grid grid-cols-4 gap-5">
                 {photos.map(photo=><AnimalCard key={photo.id} photo={photo}/>)}
             </div>
